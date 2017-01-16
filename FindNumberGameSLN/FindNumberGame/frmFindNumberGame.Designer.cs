42049219a7d506c8ad5ca36ed8f5381e68103bea
Kobi Keren <kobikms728@outlook.com>
2017-01-16 15:03:14 +0200

    created the project

---
+++ FindNumberGameSLN/FindNumberGame/frmFindNumberGame.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace FindNumberGame
+{
+    partial class frmFindNumberGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblLowerLimit = new System.Windows.Forms.Label();
+            this.lblUpperLimit = new System.Windows.Forms.Label();
+            this.lblNumberOfSteps = new System.Windows.Forms.Label();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblRules = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblLowerLimit
+            // 
+            this.lblLowerLimit.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblLowerLimit.Location = new System.Drawing.Point(162, 25);
+            this.lblLowerLimit.Name = "lblLowerLimit";
+            this.lblLowerLimit.Size = new System.Drawing.Size(100, 23);
+            this.lblLowerLimit.TabIndex = 0;
+            // 
+            // lblUpperLimit
+            // 
+            this.lblUpperLimit.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblUpperLimit.Location = new System.Drawing.Point(383, 25);
+            this.lblUpperLimit.Name = "lblUpperLimit";
+            this.lblUpperLimit.Size = new System.Drawing.Size(100, 23);
+            this.lblUpperLimit.TabIndex = 1;
+            // 
+            // lblNumberOfSteps
+            // 
+            this.lblNumberOfSteps.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblNumberOfSteps.Location = new System.Drawing.Point(162, 66);
+            this.lblNumberOfSteps.Name = "lblNumberOfSteps";
+            this.lblNumberOfSteps.Size = new System.Drawing.Size(100, 23);
+            this.lblNumberOfSteps.TabIndex = 2;
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblMessage.Location = new System.Drawing.Point(33, 244);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(450, 75);
+            this.lblMessage.TabIndex = 3;
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(278, 199);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(100, 22);
+            this.txtNumber.TabIndex = 4;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(122, 195);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(99, 31);
+            this.btnPlay.TabIndex = 5;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(384, 334);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(99, 31);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Lower limit :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(285, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Upper limit :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Number of steps :";
+            // 
+            // lblRules
+            // 
+            this.lblRules.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblRules.Location = new System.Drawing.Point(33, 107);
+            this.lblRules.Name = "lblRules";
+            this.lblRules.Size = new System.Drawing.Size(450, 69);
+            this.lblRules.TabIndex = 10;
+            // 
+            // frmFindNumberGame
+            // 
+            this.AcceptButton = this.btnPlay;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(523, 393);
+            this.Controls.Add(this.lblRules);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lblNumberOfSteps);
+            this.Controls.Add(this.lblUpperLimit);
+            this.Controls.Add(this.lblLowerLimit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "frmFindNumberGame";
+            this.Text = "Find the number - Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLowerLimit;
+        private System.Windows.Forms.Label lblUpperLimit;
+        private System.Windows.Forms.Label lblNumberOfSteps;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblRules;
+    }
+}
+
---
+++ FindNumberGameSLN/FindNumberGame/frmFindNumberGame.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FindNumberGame
+{
+    public partial class frmFindNumberGame : Form
+    {
+        private Random random;
+        private int targetNumber;
+
+        public frmFindNumberGame()
+        {
+            string rules;
+            InitializeComponent();
+
+            //set the text of the label lblRules
+            rules = "Try to find the unknown target number as fast as you can.\n\n" +
+                "Enter a number between the lower and the upper limits.";
+            lblRules.Text = rules;
+
+            //prepare the variable 'random' and prepare the first game
+            random = new Random();
+            ResetGame();
+        }
+
+        private void ResetGame()
+        {
+            //set values for the various controls and use the 'random' variable to
+            //select a target number for the new game
+            lblMessage.Text = "";
+            targetNumber = random.Next(1, 1001);
+            lblLowerLimit.Text = "1";
+            lblUpperLimit.Text = "1000";
+            lblNumberOfSteps.Text = "0";
+            txtNumber.Text = "";
+            txtNumber.Focus();
+        }
+
+        private void PrintMessage(string message)
+        {
+            //display the message
+            lblMessage.Text = message;
+            txtNumber.Focus();
+            txtNumber.SelectAll();
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            string message;
+            int lowerLimit = int.Parse(lblLowerLimit.Text);
+            int upperLimit = int.Parse(lblUpperLimit.Text);
+            int number;
+            int newNumberOfSteps = int.Parse(lblNumberOfSteps.Text) + 1;
+
+            if (txtNumber.Text == "")
+            {
+                message = "Error: invalid input\n\nThe text box is empty";
+                PrintMessage(message);
+                return;
+            }
+            
+            if (!int.TryParse(txtNumber.Text,out number))
+            {
+                message = "Error: invalid input\n\n'" + txtNumber.Text + 
+                    "' is not a number";
+                PrintMessage(message);
+                return;
+            }
+
+            number = int.Parse(txtNumber.Text);
+            if (number < lowerLimit || number > upperLimit)
+            {
+                message = "Error: invalid input\n\n'" + txtNumber.Text + 
+                    "' is out of range";
+                PrintMessage(message);
+                return;
+            }
+
+            if (number < targetNumber)
+            {
+                message = "the target number is bigger than " + number.ToString();                
+                lblLowerLimit.Text = (number + 1).ToString();
+                lblNumberOfSteps.Text = newNumberOfSteps.ToString();
+                PrintMessage(message);
+                return;
+            }
+
+            if (number > targetNumber)
+            {
+                message = "the target number is smaller than " + number.ToString();                
+                lblUpperLimit.Text = (number - 1).ToString();
+                lblNumberOfSteps.Text = newNumberOfSteps.ToString();
+                PrintMessage(message);
+                return;
+            }
+
+            if (number == targetNumber)
+            {                
+                message = "WOW !!   GOOD JOB !!\n\nThe target number is: " + 
+                    targetNumber.ToString() + "\nYou found it in " +
+                    newNumberOfSteps.ToString() + " steps";
+                lblNumberOfSteps.Text = newNumberOfSteps.ToString();
+                lblMessage.Text = message;
+                MessageBox.Show(message, "Message");
+                ResetGame();
+            }            
+        }        
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //close the application
+            Application.Exit();
+        }
+    }
+}
